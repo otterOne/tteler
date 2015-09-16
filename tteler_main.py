de84fedfa667eed7f8db11dff4daf0f581b889ba
MrFoogy <jonatlun@gmail.com>
2015-09-16 20:00:21 +0200

    added basic movement

--- tteler_main.py
+++ tteler_main.py
@@ -4,8 +4,19 @@
 import pygame
 
 
+def charInString(letter,text):
+    for c in text:
+        if c == letter:
+            return True
+    return False
+
+
 def main():
     
+
+    obstacles = "OT~|_"
+
+
     while True:
         
         
@@ -14,11 +25,22 @@ def main():
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    #Move player left
+                    face = 0
+                    if not charInString(grid[locY][locX-1],obstacles):
+                        locX -= 1
                 if event.key == pygame.K_RIGHT:
-                    #Move player right
-
-
+                    face = 2
+                    if not charInString(grid[locY][locX+1],obstacles):
+                        locX += 1
+                if event.key == pygame.K_DOWN:
+                    face = 3
+                    if not charInString(grid[locY+1][locX],obstacles):
+                        locX += 1
+                if event.key == pygame.K_UP:
+                    face = 1
+                    if not charInString(grid[locY-1][locX],obstacles):
+                        locX -= 1
+            
 
 
 
