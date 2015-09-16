f163988295f68533b5fcb05fbe3119f5ce71950a
MrFoogy <jonatlun@gmail.com>
2015-09-16 19:26:20 +0200

    imported pygame

---
+++ tteler_main.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+
+import pygame
+
+
+def main():
+    
+    while True:
+        
+        
+        #Input
+        x = 2
+
+
+
+
+
+
+
+
+
+
+
+
+        #Update
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #Rendering
+        
